190424a07d855b9a5a9147f9a8d5487084573a66
LeanAranda <arandaleandro2003@gmail.com>
2026-06-12 23:18:11 -0300

    Tipos de datos basicos

--- src/main/antlr4/MiniLang.g4
+++ src/main/antlr4/MiniLang.g4
@@ -7,9 +7,19 @@ instruccion
     ;
 
 imprimir
-    : 'print' '(' CADENA ')' ';'
+    : 'print' '(' expresion ')' ';'
     ;
 
-CADENA : '"' .*? '"' ;
+expresion
+    : CADENA
+    | ENTERO
+    | REAL
+    | BOOLEANO
+    ;
+
+CADENA   : '"' .*? '"' ;
+ENTERO   : [0-9]+ ;
+REAL     : [0-9]+ '.' [0-9]+ ;
+BOOLEANO : 'true' | 'false' ;
 
 WS : [ \t\r\n]+ -> skip ;
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,16 +5,25 @@ public class Main {
 
     public static void main(String[] args) throws Exception {
 
-        CharStream input = CharStreams.fromString("print(\"Hola\");");
+    	// Simula un archivo fuente con el código
+        CharStream input = CharStreams.fromString(
+        	    "print(\"Hola\");\n" +
+        	    "print(123);\n" +
+        	    "print(98.3);\n" +
+        	    "print(true);\n"
+        	);
 
+        // Convierte el texto en tokens
         MiniLangLexer lexer = new MiniLangLexer(input);
 
+        // Agrupa los tokens para que el parser los consuma.
         CommonTokenStream tokens = new CommonTokenStream(lexer);
 
+        // Usa la gramática para construir el árbol sintáctico.
         MiniLangParser parser = new MiniLangParser(tokens);
-
         ParseTree tree = parser.programa();
 
+        // Salida
         System.out.println(tree.toStringTree(parser));
     }
 }
